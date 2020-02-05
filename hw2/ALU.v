031eb912d098d23ee9fb0040a959eb27f0f3e109
Josue Rodriguez <josue.a.rodriguez@ttu.edu>
2020-02-05 00:25:41 -0600

    changed

--- hw2/ALU.v
+++ hw2/ALU.v
@@ -1,12 +1,27 @@
-module ALU(input A, input B, input [3:0]S, output G, output Z, output N, output O, output C );
-
+module ALU(input [15:0]A, input [15:0]B, input [3:0]S, output G, output Z, output N, output V, output C );
+    //basic logic
     case ([3:1]S):
-        3'b000: assign G = A + [0]S
-        3'b001: assign G = A + B + [0]S
-        3'b010: assign G =  A + ~B + [0]S
-        3'b011: assign G = A - 1 + [0]S
-        3'b100: assign G = A & B
-        3'b101: assign G = A | B
-        3'b110: assign G = A^B
-        3'b000: assign G = ~A + [0]S
+        3'b000: begin
+                    assign G = A+[0]S;
+                    assign V = (~[15]A & [15]G) | ([15]A & ~[15]G)
+        3'b001: begin
+                    assign G = A + B + [0]S;
+                    assign V = (~[15]A & ~[15]B & [15]G) | ([15]A & [15]B & ~[15]G);
+                    //if both of same sign and G is not overflow
+                end
+        3'b010: begin
+                    assign G =  A + ~B + [0]S;
+                    assign V = (~[15]A & [15]B & [15]G) | ([15]A & ~[15]B & ~[15]G);
+                end
+        3'b011: assign G = A - 1 + [0]S;
+        3'b100: assign G = A & B;
+        3'b101: assign G = A | B;
+        3'b110: assign G = A^B;
+        3'b111: assign G = ~A + [0]S;
     endcase
+    //zero detect
+    case (G):
+        16'b0: assign Z = 1;
+        default: Z = 0;
+    endcase
+    //overflow
