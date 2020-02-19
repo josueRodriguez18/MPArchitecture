d31cf3f2635e59b28edda6de123e153aeeaa89ca
Josue Rodriguez <josue.a.rodriguez@ttu.edu>
2020-02-19 00:03:39 -0600

    done pretty much

--- hw3/VendingMachine.v
+++ hw3/VendingMachine.v
@@ -1,7 +1,5 @@
-module VendingMachine(input [5:0]item_select, input [4:0]cash, input [4:0]coin, input [5:0]coke_inventory, input clk, input reset, output [5:0]dispend_change, output reg [5:0]dispend_coke);
+module VendingMachine(input [5:0]item_select, input [4:0]bank, input [4:0]coin, input [5:0]coke_inventory, input clk, input reset, output [5:0]dispend_change, output reg [5:0]dispend_coke);
                                         //coin input    coke_inventory inventory  clock       reset
-    reg [5:0]sel;
-    assign coke_inventory = sel;
     parameter price = 150;
     parameter running = 0;
     parameter IDLE = 0;
@@ -155,7 +153,10 @@ always @(posedge clk)
             end
     if(quarter_counter >= 6 &(coke_inventory & item_select)) //the right amount of money and item is available
             begin
-                running_change = 6 - quarter_counter;
+                //running_change = 6 - quarter_counter;
+                quarter_counter = 6 - quarter_counter;
+                running_change = state
+
                 state = VENDING;
             end
      else state = VOID_TRANSACTION;
@@ -164,6 +165,88 @@ always @(posedge clk)
         VENDING:
             begin
                 dispend_coke = item_select;
+                case(quarter_counter)
+                    2:  
+                        case(bank)
+                            5'b1xxxx: quarter_counter = quarter_counter - 4; //dollar
+                            5'b01xxx: quarter_counter = quarter_counter - 2; //half dollar
+                            5'b001xx: quarter_counter = quarter_counter - 1; //quarter
+                            5'b0001x: 
+                                begin 
+                                    case(running_change)
+                                        FIVE:
+                                            begin 
+                                                running_change = TWENTY;
+                                                quarter_counter = quarter_counter - 1;
+                                            end
+                                        TEN: 
+                                            begin
+                                                running_change = 0;
+                                                quarter_counter = quarter_counter - 1;
+                                            end 
+                                        FIFTEEN: running_change = FIVE;
+                                        TWENTY: running_change = TEN;
+                                    endcase
+                                end    
+                            5'b00001:
+                                begin
+                                    case(running_change)
+                                        begin
+                                            FIVE:
+                                                begin 
+                                                    running_change = 0;
+                                                    quarter_counter = quarter_counter - 1;
+                                                end
+                                            TEN: running_change = FIVE;
+                                            FIFTEEN: running_change = TEN;
+                                            TWENTY: running_change = FIFTEEN;
+                    1:
+                        begin
+                            case(bank)
+                                5'bxx1xx: quarter_counter = quarter_counter - 1; //quarter
+                                5'bxx01x:
+                                    begin
+                                        case(running_change)
+                                                FIVE:
+                                                    begin
+                                                        running_change = TWENTY;
+                                                        quarter_counter = quarter_counter - 1;
+                                                    end
+                                                TEN: 
+                                                    begin
+                                                        running_change = 0;
+                                                        quarter_counter = quarter_counter - 1;
+                                                    end
+                                                FIFTEEN: running_change = FIVE;
+                                                TWENTY: running_change = TEN;
+                                        endcase
+                                    end
+                        end
+                    0:
+                        begin
+                            case(bank)
+                                begin
+                                    5'bxxx10:
+                                        case(running_change)
+                                            FIVE: next_state = VOID_TRANSACTION;
+                                            TEN: running_change = 0;
+                                            FIFTEEN: running_change = FIVE;
+                                            TWENTY: running_change = TEN;
+                                        endcase
+                                    5'bxxx01:
+                                        case(running_change)
+                                            FIVE: running_change = 0;
+                                            TEN: running_change = FIVE;
+                                            FIFTEEN: running_change = TEN;
+                                            TWENTY: running_change = FIFTEEN;
+                                end
+                        end
+
+                                    
+
+                                        
+
+                next_state = VENDING;
                 
             end
         VOID_TRANSACTION:
